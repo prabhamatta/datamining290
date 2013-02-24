7d1dbee61f0fc25d149f11c8df34b3319202e0dc
PrabhaMatta <prabha.matta@gmail.com>
2013-02-23 20:16:12 -0800

    User Similarity 3rd way of doing it.

--- code/user_similarity.py
+++ code/user_similarity.py
@@ -19,19 +19,20 @@ class UserSimilarity(MRJob):
         """Output user_id, list of businesses"""
         yield [user_id, list(businesses)]
         
-    def dummy_mapper(self, user_id, list_businesses):
-        #print user_id,businesses
+    def mapper_bus_userbuslist(self, user_id, list_businesses):
         """for each user==> returns "dummy",  [user_id,Businesses]"""
-        yield ["dummy", [user_id, list_businesses]]    
+        for bus in list_businesses:
+            yield [bus, [user_id, list_businesses]]    
         
-    def create_combinations(self, dummy_var, list_of_user_businesses):
+    def reducer_groupbybus(self, bus, list_of_user_businesses):
         """ returns "dummy", list of  all [user_id,Businesses]"""
-        yield "dummy",list(list_of_user_businesses)
+        yield bus,list(list_of_user_businesses)
         
                     
-    def calculate_jaccard(self, dummy, list_of_user_businesses):
+    def mapper_calculate_jaccard(self, bus, list_of_user_businesses):
         """Output user_id1, user_id2 whose jaccard_coefficient > = 0.5"""
         #print user1_businesses,user2_businesses
+        result = set()
         for i in range(len(list_of_user_businesses)):
             for j in range(len(list_of_user_businesses)):
                 if i > j:                            
@@ -43,10 +44,16 @@ class UserSimilarity(MRJob):
                     jaccard_coefficient =  float(len(b1_set.intersection(b2_set)))/float(len(b1_set.union(b2_set)))
                     
                     #import pdb; pdb.set_trace()
-                    #yield [[ u1,u2],jaccard_coefficient]
+                    #yield (u1,u2),jaccard_coefficient
                     if jaccard_coefficient >= 0.5:
-                        yield u1,u2
-                    
+                        yield (u1,u2),1
+
+        
+    def reducer_get_unique(self, user1_user2, dummy):
+        """ returns "dummy", list of  all [user_id,Businesses]"""
+        #yield users,list(jac_coeff)[0]
+        yield user1_user2
+    
 
     def steps(self):
         """TODO: Document what you expect each mapper and reducer to produce:
@@ -55,8 +62,8 @@ class UserSimilarity(MRJob):
         mapper2: ...
         """
         return [self.mr(mapper=self.extract_users ,reducer=self.join_businesses),
-                self.mr(mapper=self.dummy_mapper,reducer=self.create_combinations),
-                self.mr(mapper= self.calculate_jaccard)]
+                self.mr(mapper=self.mapper_bus_userbuslist,reducer=self.reducer_groupbybus),
+                self.mr(mapper= self.mapper_calculate_jaccard, reducer=self.reducer_get_unique)]
 
 
 if __name__ == '__main__':
