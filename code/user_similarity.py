b9db2a6f0ef5656210c95d1d4b66858ce69a670c
PrabhaMatta <prabha.matta@gmail.com>
2013-02-23 18:36:44 -0800

    User Similarity one more way of doing...using 'dummy'

--- code/user_similarity.py
+++ code/user_similarity.py
@@ -17,43 +17,36 @@ class UserSimilarity(MRJob):
             
     def join_businesses(self, user_id, businesses):
         """Output user_id, list of businesses"""
-        yield [user_id, 2]#businesses]
+        yield [user_id, list(businesses)]
         
     def dummy_mapper(self, user_id, list_businesses):
         #print user_id,businesses
-        """ returns "dummy", list of [user_id,Businesses]"""
+        """for each user==> returns "dummy",  [user_id,Businesses]"""
         yield ["dummy", [user_id, list_businesses]]    
         
     def create_combinations(self, dummy_var, list_of_user_businesses):
-        mydict = {}
-        for u, b in list_of_user_businesses:
-            if u not in mydict:
-                mydict[u] = [b]
-            else:
-                mydict[u].append(b)
-                
-        myUserBussList = []
-        for u, bs in mydict.items():
-            myUserBussList.append([u, bs])
+        """ returns "dummy", list of  all [user_id,Businesses]"""
+        yield "dummy",list(list_of_user_businesses)
         
-        for i in range(len(myUserBussList)):
-            for j in range(len(myUserBussList)):
-                if i > j:
-                    yield [myUserBussList[i], myUserBussList[j]]   
                     
-    def calculate_jaccard(self, user1_businesses, user2_businesses):
+    def calculate_jaccard(self, dummy, list_of_user_businesses):
         """Output user_id1, user_id2 whose jaccard_coefficient > = 0.5"""
         #print user1_businesses,user2_businesses
-        u1, b1 = user1_businesses
-        u2, b2 = user2_businesses
-
-        b1_set = set(b1) 
-        b2_set = set(b2)
-        
-        jaccard_coefficient = float(len(b1_set.intersection(b2_set))/len(b1_set.union(b2_set)))      
-        if jaccard_coefficient >= 0.5:
-            yield [ u1,u2]
-        
+        for i in range(len(list_of_user_businesses)):
+            for j in range(len(list_of_user_businesses)):
+                if i > j:                            
+                    u1,b1 = list_of_user_businesses[i]
+                    u2,b2 = list_of_user_businesses[j]
+                    b1_set = set(b1) 
+                    b2_set = set(b2)
+                    
+                    jaccard_coefficient =  float(len(b1_set.intersection(b2_set)))/float(len(b1_set.union(b2_set)))
+                    
+                    #import pdb; pdb.set_trace()
+                    #yield [[ u1,u2],jaccard_coefficient]
+                    if jaccard_coefficient >= 0.5:
+                        yield u1,u2
+                    
 
     def steps(self):
         """TODO: Document what you expect each mapper and reducer to produce:
@@ -61,9 +54,9 @@ class UserSimilarity(MRJob):
         reducer1: <key, [values]>
         mapper2: ...
         """
-        return [self.mr(mapper=self.extract_users ),#reducer=self.join_businesses)]
-                self.mr(mapper=self.dummy_mapper, reducer=self.create_combinations),
-                self.mr(mapper=self.calculate_jaccard)]
+        return [self.mr(mapper=self.extract_users ,reducer=self.join_businesses),
+                self.mr(mapper=self.dummy_mapper,reducer=self.create_combinations),
+                self.mr(mapper= self.calculate_jaccard)]
 
 
 if __name__ == '__main__':
